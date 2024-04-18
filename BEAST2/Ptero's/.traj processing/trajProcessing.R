35a6e7722f39ee4a129fdbb502bddb0b6e02ad23
TheRapto <joel.koelewijn19999@gmail.com>
2024-04-18 16:51:15 +0200

    New commit
    A commit to make sure all the most recent work is preserved.

--- BEAST2/Ptero's/.traj processing/trajProcessing.R
+++ BEAST2/Ptero's/.traj processing/trajProcessing.R
@@ -26,7 +26,7 @@ parseTrajectory <- function(trajStr) {
 }
 
 #Loading in the original trajectory file for processing:
-Traj= read_tsv("constant rate FBD - Mkp - age uncertainty - linked occurences - Initial - 30-11.New Pterosaur Matrix.traj", col_types="ic")
+Traj= read_tsv("Changing Birth-death FBD - Mkp - Uncertain ages - 06-02.New Pterosaur Matrix.traj", col_types="ic")
 
 #The function that processes the trajectroies, adapted to handle preloaded Traj's
 loadTrajectories <- function(filename, burninFrac=0.1, subsample=NA) {
@@ -89,18 +89,21 @@ loadTrajectories <- function(filename, burninFrac=0.1, subsample=NA) {
 }
 
 #Runs the traj processor for the loaded in Traj:
-Processed_Traj=loadTrajectories("Ohno")
+Processed_Traj=loadTrajectories("Traj")
 #Saves the processed traj to the wanted name:
-save(Processed_Traj, file="Processed_Traj_Constant.Rdata")
+save(Processed_Traj, file="Processed_Traj_BD.Rdata")
 
 
 ####Changing all rates plot:####
+
+#Set amount of subsampling. For example 10 means every tenth value of the trajectory.
+Subsample=100
 {
 #Loads the wanted processed Traj. Choose one:
 load("Processed_Traj_AllRates.Rdata")
 #Parsing the inputs to be readable
-Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), 10)]
-N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), 10)]
+Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), Subsample)]
+N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
 df=data.frame(y=N,
               x=Time)
 title="Changing All Rates"
@@ -122,11 +125,11 @@ plot1
 {
 load("Processed_Traj_Sampling.Rdata")
 #Parsing the inputs to be readable
-Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), 10)]
-N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), 10)]
+Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), Subsample)]
+N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
 df=data.frame(y=N,
              x=Time)
-title="Changing Sampling"
+title="Changing Sampling rate"
 #The plot
 plot2=ggplot(data = df, aes(x=x, y = y)) +
   geom_point() +
@@ -145,8 +148,8 @@ plot2
 {
 load("Processed_Traj_Constant.Rdata")
 #Parsing the inputs to be readable
-Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), 10)]
-N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), 10)]
+Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), Subsample)]
+N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
 df=data.frame(y=N,
               x=Time)
 title="Constant rates"
@@ -165,12 +168,173 @@ plot3=ggplot(data = df, aes(x=x, y = y)) +
 plot3
 }
 
+####Changing birth-death rates plot:###
+{
+  load("Processed_Traj_BD.Rdata")
+  #Parsing the inputs to be readable
+  Time=Processed_Traj$states$time[seq(1, length(Processed_Traj$states$time), Subsample)]
+  N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
+  df=data.frame(y=N,
+                x=Time)
+  title="Changing Birth-Death rates"
+  #The plot
+  plot4=ggplot(data = df, aes(x=x, y = y)) +
+    geom_point() +
+    ggtitle(title)+ #for the title
+    ylab("N")+ # for the x axis label
+    xlab("Time (Myr)")+ # for the y axis label
+    theme_bw()+ #Makes the background white.
+    theme(text = element_text(size = 6), plot.tag = element_text(face = "bold"), plot.tag.position = c(0.01, 0.98)) +
+    theme(legend.position = c(0.3, 0.85)) +
+    theme(legend.key.size = unit(0.3, "cm")) +
+    theme(legend.title = element_blank()) 
+  
+  plot4
+}
+
 #The multiplot
 {
 default_width_fp_in = 170 / 25.4 # width for  full page fig in inch
 file_name = paste("figs/Trajectoryplots_raw.pdf")
 pdf(file = file_name , width=default_width_fp_in, height = 29.7/2.54)
-combined_plot=gridExtra::grid.arrange(plot3,plot2,plot1, ncol=1)
+combined_plot=gridExtra::grid.arrange(plot3,plot2,plot4,plot1, ncol=1)
 dev.off()
 }
 
+
+####Changing all rates 95% HPD plot:####
+
+#Set amount of subsampling. For example 10 means every tenth value of the trajectory.
+Subsample=100
+{
+  #Loads the wanted processed Traj. Choose one:
+  load("Processed_Traj_AllRates.Rdata")
+  #Parsing the inputs to be readable and getting the density curve for N
+  N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
+  NDensity = density(N)
+  df=data.frame(y=NDensity$y,
+                x=NDensity$x)
+  title="Changing All Rates"
+
+  # Numerically find HPDI
+  const = sum(NDensity$y)
+  spxx = sort(NDensity$y, decreasing = TRUE) / const
+  crit = spxx[which(cumsum(spxx) >=0.95)[1]]*const
+  
+  #The plot
+  plot1=ggplot(data = df, aes(x=x, y = y)) +
+    geom_line() +
+    geom_hline(yintercept=crit, linetype="dashed", color = "red")+
+    ggtitle(title)+ #for the title
+    ylab("Density")+ # for the x axis label
+    xlab("N value")+ # for the y axis label
+    theme_bw()+ #Makes the background white.
+    theme(text = element_text(size = 6), plot.tag = element_text(face = "bold"), plot.tag.position = c(0.01, 0.98)) +
+    theme(legend.position = c(0.3, 0.85)) +
+    theme(legend.key.size = unit(0.3, "cm")) +
+    theme(legend.title = element_blank())+
+    scale_x_continuous(limits=c(-10,150))
+  
+  plot1
+}
+####Changing sampling plot:####
+{  
+  load("Processed_Traj_Sampling.Rdata")
+  #Parsing the inputs to be readable and getting the density curve for N
+  N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
+  NDensity = density(N)
+  df=data.frame(y=NDensity$y,
+                x=NDensity$x)
+  title="Changing Sampling rate"
+  
+  # Numerically find HPDI
+  const = sum(NDensity$y)
+  spxx = sort(NDensity$y, decreasing = TRUE) / const
+  crit = spxx[which(cumsum(spxx) >=0.95)[1]]*const
+  
+  #The plot
+  plot2=ggplot(data = df, aes(x=x, y = y)) +
+    geom_line() +
+    geom_hline(yintercept=crit, linetype="dashed", color = "red")+
+    ggtitle(title)+ #for the title
+    ylab("Density")+ # for the x axis label
+    xlab("N value")+ # for the y axis label
+    theme_bw()+ #Makes the background white.
+    theme(text = element_text(size = 6), plot.tag = element_text(face = "bold"), plot.tag.position = c(0.01, 0.98)) +
+    theme(legend.position = c(0.3, 0.85)) +
+    theme(legend.key.size = unit(0.3, "cm")) +
+    theme(legend.title = element_blank()) +
+    scale_x_continuous(limits=c(-10,150))
+  
+  plot2
+}
+####Constant rates plot:###
+{  
+  load("Processed_Traj_Constant.Rdata")
+  #Parsing the inputs to be readable and getting the density curve for N
+  N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
+  NDensity = density(N)
+  df=data.frame(y=NDensity$y,
+                x=NDensity$x)
+  title="Constant rates"
+  # Numerically find HPDI
+  const = sum(NDensity$y)
+  spxx = sort(NDensity$y, decreasing = TRUE) / const
+  crit = spxx[which(cumsum(spxx) >=0.95)[1]]*const
+  
+  #The plot
+  plot3=ggplot(data = df, aes(x=x, y = y)) +
+    geom_line() +
+    geom_hline(yintercept=crit, linetype="dashed", color = "red")+
+    ggtitle(title)+ #for the title
+    ylab("Density")+ # for the x axis label
+    xlab("N value")+ # for the y axis label
+    theme_bw()+ #Makes the background white.
+    theme(text = element_text(size = 6), plot.tag = element_text(face = "bold"), plot.tag.position = c(0.01, 0.98)) +
+    theme(legend.position = c(0.3, 0.85)) +
+    theme(legend.key.size = unit(0.3, "cm")) +
+    theme(legend.title = element_blank())+
+    scale_x_continuous(limits=c(-10,150)) 
+  
+  plot3
+}
+
+####Changing birth-death rates plot:###
+{
+  load("Processed_Traj_BD.Rdata")
+  #Parsing the inputs to be readable and getting the density curve for N
+  N=Processed_Traj$states$N[seq(1, length(Processed_Traj$states$N), Subsample)]
+  NDensity = density(N)
+  df=data.frame(y=NDensity$y,
+                x=NDensity$x)
+  title="Changing Birth-Death rates"
+  # Numerically find HPDI
+  const = sum(NDensity$y)
+  spxx = sort(NDensity$y, decreasing = TRUE) / const
+  crit = spxx[which(cumsum(spxx) >=0.95)[1]]*const
+  
+  #The plot
+  plot4=ggplot(data = df, aes(x=x, y = y)) +
+    geom_line() +
+    geom_hline(yintercept=crit, linetype="dashed", color = "red")+
+    ggtitle(title)+ #for the title
+    ylab("Density")+ # for the x axis label
+    xlab("N value")+ # for the y axis label
+    theme_bw()+ #Makes the background white.
+    theme(text = element_text(size = 6), plot.tag = element_text(face = "bold"), plot.tag.position = c(0.01, 0.98)) +
+    theme(legend.position = c(0.3, 0.85)) +
+    theme(legend.key.size = unit(0.3, "cm")) +
+    theme(legend.title = element_blank())+
+    scale_x_continuous(limits=c(-10,150)) 
+  
+  plot4
+}
+
+#The multiplot
+{
+  default_width_fp_in = 170 / 25.4 # width for  full page fig in inch
+  file_name = paste("figs/Densityplots_raw.pdf")
+  pdf(file = file_name , width=default_width_fp_in, height = 29.7/2.54)
+  combined_plot=gridExtra::grid.arrange(plot3,plot2,plot4,plot1, ncol=1)
+  dev.off()
+}
